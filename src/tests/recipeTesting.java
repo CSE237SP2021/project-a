262e0a325b05b2150dd474983b892900061a00c2
jina park <jinapark@localhost>
2021-03-29 13:01:31 -0500

    added unit tests

--- src/tests/recipeTesting.java
+++ src/tests/recipeTesting.java
@@ -80,5 +80,33 @@ class recipeTesting {
 		assertEquals(InventoryTester.getAllInventory(), mockIngredientList);
 	}
 	
+	@Test
+	//Unit test to test adding same ingredient
+	void testAddingSameIngredient() {
+		InventoryManagement InventoryTester = new InventoryManagement();
+		InventoryTester.addFood("banana", 10.0);
+		InventoryTester.addFood("banana", 10.0);
+		
+		HashMap <String, Double> correctMockInventory = new HashMap <String, Double> ();
+		correctMockInventory.put("banana", (double) 10);
+		correctMockInventory.put("banana", (double) 10);
+	
+		assertEquals(InventoryTester.getInstance(), correctMockInventory);
+		
+	}
+	
+	@Test
+	//Unit test to test removing same ingredient
+	void testRemovingSameIngredient() {
+		InventoryManagement InventoryTester = new InventoryManagement();
+		InventoryTester.addFood("banana", 25.0);
+		InventoryTester.removeFood("banana", 5.0);
+		InventoryTester.removeFood("banana", 10.0);
+		
+		HashMap <String, Double> correctMockInventory = new HashMap <String, Double> ();
+		correctMockInventory.put("banana", (double) 10);
+	
+		assertEquals(InventoryTester.getInstance(), correctMockInventory);
+	}
 
 }

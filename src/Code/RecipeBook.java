a6020a84508ed07acc8565c56457412c7888a95b
Jeevun Kanupada <jeevunkanupada@jeevuns-mbp.dhcp.wustl.edu>
2021-04-25 16:58:07 -0500

    Recipe Book add/ remove with tests

--- src/Code/RecipeBook.java
+++ src/Code/RecipeBook.java
@@ -4,7 +4,42 @@ import java.util.LinkedList;
 
 public class RecipeBook {
 	
-	public LinkedList<Recipe> allRecipes;
+	public LinkedList<Recipe> allRecipes= new LinkedList<Recipe>();
 	
-
+	public void addRecipe(Recipe a) {
+		if (allRecipes.contains(a)) {
+			return;
+		}
+		else
+	
+		allRecipes.add(a);
+		
+		
+		return;
+	}
+	public void removeRecipe(Recipe a) {
+		if (allRecipes.contains(a)) {
+			allRecipes.remove(a);
+		}
+		return;
+	}
+	public boolean recipeContains(Recipe a) {
+		return allRecipes.contains(a);
+		
+	}
+	public LinkedList<String> listRecipes(){
+		LinkedList<String> RecipeList = new LinkedList<String>();
+		for(int i=0; i< allRecipes.size(); i++) {
+			RecipeList.add(allRecipes.get(i).name());
+			System.out.println(RecipeList.get(i));
+		}
+		return RecipeList;
+	}
+	
+	
+	public void  sortByIngredient(InventoryManagement Inventory,Recipe a) {
+		
+	
+	}
+	
 }
--- src/tests/RecipeBookTesting.java
+++ src/tests/RecipeBookTesting.java
@@ -1,13 +1,107 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
 import Code.Recipe;
+import Code.RecipeBook;
 
 public class RecipeBookTesting {
 	
 	
-
-	void addRecipe(Recipe a) {
+	@Test
+	 public void TestAddRecipe() {
+		RecipeBook Book= new RecipeBook();
+		//String instructions, LinkedList<String> ingredients, int recipeDuration, boolean isRecipeAdvanced
+		LinkedList<String> PBJtestIngredients = new LinkedList<String>();
+		PBJtestIngredients.add("peanut butter");
+		PBJtestIngredients.add("jelly");
+		PBJtestIngredients.add("bread");
 		
+		Recipe PBJ = new Recipe("PBJ","Spread peanut butter on a slice of bread. Spread jelly on another slice of bread. "
+				+ "Put the two slides together, with the pb and jelly sides facing each other.",PBJtestIngredients,10, false );
+		
+		
+		LinkedList<String> ChickenSoupIngredients = new LinkedList<String>();
+		ChickenSoupIngredients.add("Chicken Broth");
+		ChickenSoupIngredients.add("Chicken");
+		ChickenSoupIngredients.add("Noodles");
+		ChickenSoupIngredients.add("Carrots");
+		ChickenSoupIngredients.add("Celery");
+		
+		Recipe ChickenSoup = new Recipe("Chicken Soup","Heat oil. Add carrots, celery, and Onion. Then Broth and chicken. And cook mmmmm.",ChickenSoupIngredients,30, true );
+		
+		
+		LinkedList<String> FancySteakIngredients = new LinkedList<String>();
+		FancySteakIngredients.add("Beef");
+		FancySteakIngredients.add("Garlic");
+		FancySteakIngredients.add("Butter");
+		
+		Recipe FancySteak = new Recipe("Fancy Steak", "Heat stove. Add Garlic. Add steak. Cook until medium rare(about 10 min. Add Butter on top",FancySteakIngredients,30, true );
+		
+		
+		
+				
+		Book.addRecipe(PBJ);
+		
+		Book.addRecipe(ChickenSoup);
+		Book.addRecipe(FancySteak);
+		
+	
+		LinkedList<Recipe> testBook= new LinkedList<Recipe>();
+		testBook.add(PBJ);
+		testBook.add(ChickenSoup);
+		testBook.add(FancySteak);
+		
+		assertEquals(testBook, Book.allRecipes);
+		
+	}
+	@Test
+	 public void TestRemoveRecipe() {
+		RecipeBook Book= new RecipeBook();
+		LinkedList<String> PBJtestIngredients = new LinkedList<String>();
+		PBJtestIngredients.add("peanut butter");
+		PBJtestIngredients.add("jelly");
+		PBJtestIngredients.add("bread");
+		
+		Recipe PBJ = new Recipe("PBJ","Spread peanut butter on a slice of bread. Spread jelly on another slice of bread. "
+				+ "Put the two slides together, with the pb and jelly sides facing each other.",PBJtestIngredients,10, false );
+		
+		
+		LinkedList<String> ChickenSoupIngredients = new LinkedList<String>();
+		ChickenSoupIngredients.add("Chicken Broth");
+		ChickenSoupIngredients.add("Chicken");
+		ChickenSoupIngredients.add("Noodles");
+		ChickenSoupIngredients.add("Carrots");
+		ChickenSoupIngredients.add("Celery");
+		
+		Recipe ChickenSoup = new Recipe("Chicken Soup","Heat oil. Add carrots, celery, and Onion. Then Broth and chicken. And cook mmmmm.",ChickenSoupIngredients,30, true );
+		
+		
+		LinkedList<String> FancySteakIngredients = new LinkedList<String>();
+		FancySteakIngredients.add("Beef");
+		FancySteakIngredients.add("Garlic");
+		FancySteakIngredients.add("Butter");
+		
+		Recipe FancySteak = new Recipe("Fancy Steak", "Heat stove. Add Garlic. Add steak. Cook until medium rare(about 10 min. Add Butter on top",FancySteakIngredients,30, true );
+		
+		
+		
+				
+		Book.addRecipe(PBJ);
+		Book.addRecipe(FancySteak);
+		Book.removeRecipe(PBJ);
+		Book.removeRecipe(ChickenSoup);
+		
+	
+		LinkedList<Recipe> testBook= new LinkedList<Recipe>();
+		
+		testBook.add(FancySteak);
+		
+		assertEquals(testBook, Book.allRecipes);
 		
 	}
 }
